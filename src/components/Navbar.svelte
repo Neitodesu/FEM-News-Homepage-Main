33dea59f2f8be6044646d7b71d2c155e82526b36
Neitodesu <neitodesu92@gmail.com>
2023-10-29 12:47:50 -0400

    first commit

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,46 @@
+<script>
+  let src = 'images/logo.svg';
+</script>
+
+<header class="container">
+  <div>
+    <img src={src} alt="">
+  </div>
+  <div class="nav-links">
+    <li><a href="/">Home</a></li>
+    <li><a href="/">New</a></li>
+    <li><a href="/">Popular</a></li>
+    <li><a href="/">Trending</a></li>
+    <li><a href="/">Categories</a></li>
+  </div>
+</header>
+
+<style lang="scss">
+  .container{
+    width: 100%;
+    padding-block: 1em;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    img{
+      min-width: 68px;
+    }
+
+    .nav-links{
+      display: flex;
+      gap: 2.35em;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  a{
+    color: var(--darkGrayishBlue);
+    text-decoration: none;
+
+    &:hover{
+      color: var(--softRed);
+    }
+  }
+</style>
